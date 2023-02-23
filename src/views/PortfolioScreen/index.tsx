7bb8b9ebe7b43da2ed50790ccf9d9c56c867f3ef
Chris <chris.gan0831@gmail.com>
2023-02-23 13:31:01 +1100

    portfolio card styling enhacement

--- src/views/PortfolioScreen/index.tsx
+++ src/views/PortfolioScreen/index.tsx
@@ -9,7 +9,7 @@ const Portfolio = () => {
             <Typography variant="h5" sx={{ fontFamily: "'Poopins', sans-serif", color: 'inherit', fontWeight: 600, pt: 2 }}>
                 PORTFOLIO
             </Typography>
-            <Box display="flex" sx={{ pt: 2, flexDirection: { xs: 'column', md: 'row' } }}>
+            <Box display="flex" sx={{ pt: 2, flexDirection: { xs: 'column', md: 'row' }, flexWrap: 'wrap' }}>
                 <PortfolioCard
                     title="Duplicated Social Website"
                     description="This is just a static website trying to duplicate Facebook focus mainly on the application of Formik and Yup validation on the login page "
